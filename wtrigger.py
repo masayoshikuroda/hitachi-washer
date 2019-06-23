d0289cec01dac666990538094292cc87ce6b090b
Masayoshi Kuroda <kuroda.masayoshi@gmail.com>
2019-06-23 10:49:52 +0900

    add argparser
    	modified:   wtrigger.py

--- wtrigger.py
+++ wtrigger.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from argparse import ArgumentParser
 
-threshold = 10
-message = '洗濯が完了しました。'
+usage = 'Usage: python {} [-t threshold]'
+argparser = ArgumentParser(usage=usage)
+argparser.add_argument('-t', '--threshold', type=float, dest='threshold', default=40,        help='trigger threshold')
+argparser.add_argument('-m', '--message',   type=str,   dest='message',   default='Trigger', help='itrigger message')
+args = argparser.parse_args()
 
-for line in sys.stdin:
+threshold = args.threshold
+message = args.message
+
+print >> sys.stderr, 'threashhold: ' + str(threshold)
+print >> sys.stderr, 'message;     ' + message
+while True:
+	line = sys.stdin.readline()
 	items = line.split()
 	correl = float(items[0])
 	if (correl < threshold):
